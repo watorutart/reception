0802d138f4267bc932219552cb4f82304ed39dfa
watorutart <46208434+watorutart@users.noreply.github.com>
2018-12-28 11:53:53 +0900

    Create Save.java

---
+++ Save.java
@@ -0,0 +1,18 @@
+package reception;
+
+class Save{
+	//private final String name;
+	
+	protected enum save{
+		deliCompFrglNum
+		
+
+		
+		/*private Save(String name){
+			this.name = name;
+		}*/
+		
+	};
+	
+}
+
